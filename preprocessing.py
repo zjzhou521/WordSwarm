f4958d590a4042881f5a1bc409c209daaf6dc47c
Jayson <zjzhou521@gmail.com>
2022-04-18 01:41:06 -0500

    done with output big matrix for wordswarm

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,7 @@
 import datetime
 import string
 import nltk
+import os
 
 nltk.download('stopwords')
 from nltk.corpus import stopwords
@@ -140,9 +141,11 @@ def get_term_document_matrix(docs_term_sequence):
     # print("\n")
     return term_doc_matrix
 
-def readDate():
+def readData():
     tweets = []
     totalDocs = []
+    keyword_tweets = {} # term, docList
+    keywords = []
     attributes = ["Keywords", "TweetID", "AuthorID", "Hashtags", "Tweet Created at", "Tweet Text", "Likes", "Reply",
                   "Retweets", "Polarity"]
     for k in range(6):
@@ -152,51 +155,105 @@ def readDate():
             for j in range(len(attributes)):
                 curTweet.append(data[attributes[j]][i])
             tweets.append(curTweet)
-            totalDocs.append(curTweet[5])
-    tweets.sort(key=lambda x: x[4])  # sort based on time
-    return tweets, totalDocs
-
+            doc = curTweet[5]
+            totalDocs.append(doc)
+            keyword = str(curTweet[0])
+            keyword = keyword[1:]
+            if keyword not in keywords:
+                keywords.append(keyword)
+                keyword_tweets[keyword] = []
+            keyword_tweets[keyword].append(curTweet)
+    for key in keyword_tweets.keys():
+        keyword_tweets[key].sort(key=lambda x: x[4])  # each keyword's tweets based on time
+    # tweets.sort(key=lambda x: x[4])  # sort based on time
+    return tweets, totalDocs, keyword_tweets
+def getWordTimeList():
+    wordTimeList = []
+    timeList = ["2022-01-27 22:30:00", "2022-02-23 04:00:00"]
+    startTime = time.strptime(timeList[0], "%Y-%m-%d %H:%M:%S")
+    startTime = datetime.datetime(startTime[0], startTime[1], startTime[2], startTime[3], startTime[4], startTime[5])
+    endTime = time.strptime(timeList[1], "%Y-%m-%d %H:%M:%S")
+    endTime = datetime.datetime(endTime[0], endTime[1], endTime[2], endTime[3], endTime[4], endTime[5])
+    curTime = startTime
+    cnt = 0
+    while curTime < endTime:
+        cnt += 1
+        # need tweets from [index, curTime)
+        curTime += datetime.timedelta(minutes=30)
+        wordTimeList.append(curTime)
+    return wordTimeList
+def createKeywordCSV(keywords):
+    os.system("rm timeData/*")
+    for keyword in keywords:
+        filename = "./timeData/" + keyword + ".csv"
+        tmp = []
+        dataframe = pd.DataFrame()
+        for wtime in wordTimeList:
+            dataframe[wtime] = 0
+        dataframe.to_csv(filename, index=False, sep=',')
 
 # read in data
-tweets, totalDocs = readDate()
+tweets, totalDocs, keyword_tweets = readData()
 lenTweets = len(tweets)
-print(lenTweets)  # 110900
+# print(lenTweets)  # 110900
+# print(len(keyword_tweets))  # 29
 
-# get all terms from totalDocs
-# docs_term_sequence = preprocessing(totalDocs)  # preprocess using: punctuation removal, stop word removal, case-folding, lemmatization, stemming
-# all_terms = get_term_list(docs_term_sequence) # get all terms
-# print(all_terms)
-# print(len(all_terms))
+# generate wordTimeList
+wordTimeList = getWordTimeList()  # len = 1259
+# createKeywordCSV(keyword_tweets.keys()) # create csv file based on keyword
 
-# for every 30 min, get word frequency
-timeList = ["2022-01-27 22:30:00", "2022-02-23 04:00:00"]
-startTime = time.strptime(timeList[0], "%Y-%m-%d %H:%M:%S")
-startTime = datetime.datetime(startTime[0], startTime[1], startTime[2], startTime[3], startTime[4], startTime[5])
-endTime = time.strptime(timeList[1], "%Y-%m-%d %H:%M:%S")
-endTime = datetime.datetime(endTime[0], endTime[1], endTime[2], endTime[3], endTime[4], endTime[5])
-curTime = startTime
-index = 0
-docs = []
-cnt = 0
-while curTime < endTime:
-    # need tweets from [index, curTime)
-    curTime += datetime.timedelta(minutes=30)
-    print(curTime)
-    # get docs
+# for each keyword
+for keyword in keyword_tweets.keys():
+    tweets = keyword_tweets[keyword]
+    # generate total terms-times
     docs = []
-    while index < lenTweets:
-        tweetTime = time.strptime(tweets[index][4][:19], "%Y-%m-%d %H:%M:%S")
-        tweetTime = datetime.datetime(tweetTime[0], tweetTime[1], tweetTime[2], tweetTime[3], tweetTime[4],
-                                      tweetTime[5])
-        if tweetTime >= curTime: break
-        docs.append(tweets[index][5])  # add tweet text into docs
-        index += 1
-    # preprocess docs
+    all_terms = []
+    for tw in tweets:
+        docs.append(tw[5])
     docs_term_sequence = preprocessing(docs)  # preprocess using: punctuation removal, stop word removal, case-folding, lemmatization, stemming
-    term_doc_matrix = get_term_document_matrix(docs_term_sequence)  # get term-document incidence matrix
-    cnt += 1
-    print()
-    if cnt > 1000: break  # only display front 1000
+    terms_times = {}
+    for doc_terms in docs_term_sequence:
+        for term in doc_terms:
+            if term not in terms_times.keys():
+                terms_times[term] = []
+    # for every 30 min, get word frequency
+    index = 0
+    lenTweets = len(tweets)
+    for curTime in wordTimeList:
+        # need tweets from [index, curTime)
+        print(str(index) + "/" + str(lenTweets))
+        # get docs
+        timeDocs = []
+        while index < lenTweets:
+            tweetTime = time.strptime(tweets[index][4][:19], "%Y-%m-%d %H:%M:%S")
+            tweetTime = datetime.datetime(tweetTime[0], tweetTime[1], tweetTime[2], tweetTime[3], tweetTime[4],
+                                          tweetTime[5])
+            if tweetTime >= curTime: break
+            timeDocs.append(tweets[index][5])  # add tweet text into timeDocs
+            index += 1
+        docs_term_sequence = preprocessing(timeDocs)  # preprocess using: punctuation removal, stop word removal, case-folding, lemmatization, stemming
+        term_doc_matrix = get_term_document_matrix(docs_term_sequence)  # get term-document incidence matrix
+        # log the word freq to big matrix
+        for key in terms_times.keys():
+            if key in term_doc_matrix: terms_times[key].append(term_doc_matrix[key])
+            else: terms_times[key].append(0)
+    # output to csv
+    import csv
+    filename = "./timeData/" + keyword + ".csv"
+    os.system("rm " + filename)
+    data = []
+    for key in terms_times.keys():
+        tmpList = terms_times[key]
+        tmpList.insert(0, key)
+        data.append(tmpList)
+    columnNames = []
+    columnNames.append("terms")
+    for tmp in wordTimeList: columnNames.append(tmp)
+    with open(filename, "w") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(columnNames)  # write column name
+        writer.writerows(data)  # write 2-d list
+
 
 
 
